eaaab8d117d0c04692dd8412e6a731c33ba487eb
Jacek <jacek@ivolution.pl>
2015-02-26 14:01:52 +0100

    processing only new files

--- basic_cms/management/commands/compress_cms_images.py
+++ basic_cms/management/commands/compress_cms_images.py
@@ -6,6 +6,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand  # , CommandError
 from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from basic_cms import settings
 from image_diet import squeeze
 
@@ -15,15 +16,15 @@ class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
         make_option(
-            "--recently_changed",
+            "--new_only",
             action='store_true',
             default=False,
-            dest='recently_changed',
-            help='Compress only images changed during last 24 hours',
+            dest='new_only',
+            help='Compress only new images',
         ),
     )
 
-    def handle(self, *args, **options):
+    def handle(self, new_only, **options):
         timestamp = time.time()
         timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('-%Y-%m-%d-%H:%M:%S')
 
@@ -51,17 +52,42 @@ class Command(BaseCommand):
                     default_storage.save(path, tmpfilehandle)
                     os.remove(tmpfilepath)
 
-        def compress_files(data, dirtree):
+        def compress_files(data, dirtree, new_only=False):
+            django_basic_cms_compressed_flag = "dbc_compressed"
+
             for f in data[1]:  # files from listdir
-                if f and f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):  # sometimes it is [u'']
-                    path = os.sep.join(dirtree)
-                    path = os.path.join(path, f)
-                    process_file(path)
+
+                if f and f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):  # sometimes if == [u'']
+                    dir_path = os.sep.join(dirtree)
+                    path = os.path.join(dir_path, f)
+                    flagged_file_name = '.%s.%s' % (f, django_basic_cms_compressed_flag)
+                    flag_path = os.path.join(dir_path, flagged_file_name)
+
+                    if new_only:
+                        should_process_file = False
+
+                        if not default_storage.exists(flag_path):
+                            should_process_file = True
+                        else:
+                            file_mt = default_storage.modified_time(path)
+                            flag_mt = default_storage.modified_time(flag_path)
+                            if flag_mt < file_mt:
+                                should_process_file = True
+
+                        if should_process_file:
+                            process_file(path)
+                    else:
+                        process_file(path)
+
+                    # add flag, for all files. This flag is used only when "new_only" option is called.
+                    if default_storage.exists(flag_path):
+                        default_storage.delete(flag_path)
+                    default_storage.save(flag_path, ContentFile(""))
 
             for d in data[0]:  # directories from list_dir
                 dirtree.append(d)
                 d = default_storage.listdir(os.sep.join(dirtree))
-                compress_files(d, dirtree)
+                compress_files(d, dirtree, new_only)
                 dirtree.pop()  # remove last item, not needed anymore
 
         if 'image_diet' in settings.INSTALLED_APPS and settings.BASIC_CMS_COMPRESS_IMAGES:
@@ -70,4 +96,4 @@ class Command(BaseCommand):
                 if directory:
                     dirtree = [directory]
                     data = default_storage.listdir(directory)
-                    compress_files(data, dirtree)
+                    compress_files(data, dirtree, new_only)
