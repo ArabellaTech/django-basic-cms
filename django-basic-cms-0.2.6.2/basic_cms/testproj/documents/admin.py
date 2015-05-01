730808965d50905b8747b5b592735f6e2115f03a
Jacek <jacek@ivolution.pl>
2015-05-01 21:40:18 +0200

    clenup

--- django-basic-cms-0.2.6.2/basic_cms/testproj/documents/admin.py
+++
@@ -1,9 +0,0 @@
-# Admin bindings
-from django.contrib import admin
-from basic_cms.testproj.documents.models import Document
-
-class DocumentAdmin(admin.ModelAdmin):
-
-    list_display   = ('title', 'page',)
-
-admin.site.register(Document, DocumentAdmin)
