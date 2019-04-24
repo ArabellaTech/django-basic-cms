e2bd9b08960ae49a5e2186619e590b06a01ad461
Mukesh Kitawat <mkitawat@voxiva.com>
2019-04-24 03:02:58 -0400

    Retrict django below 2.0

--- basic_cms/__init__.py
+++ basic_cms/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Django Basic CMS module."""
-VERSION = (0, 4, 0)
+VERSION = (0, 4, 1)
 __version__ = '.'.join(map(str, VERSION))
 __author__ = "ArabellaTech"
 __contact__ = "geeks@arabel.la"
